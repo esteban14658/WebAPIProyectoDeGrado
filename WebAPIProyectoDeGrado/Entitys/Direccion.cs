86a30fc280ac874da9bcb52cb3e5f0cdd908d71f
esteban14658 <esteban_14658@hotmail.com>
2021-08-03 11:45:10 -0500

    No se han podido nulear esos campos

--- WebAPIProyectoDeGrado/Entitys/Direccion.cs
+++ WebAPIProyectoDeGrado/Entitys/Direccion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,8 +17,9 @@ namespace WebAPIProyectoDeGrado.Entitys
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 25, ErrorMessage = "El campo {0} debe ser menor a {1} caracteres")]
         public string Descripcion { get; set; }
-        public Usuario Usuario { get; set; }
-        [Key, ForeignKey("Tienda")]
+        [ForeignKey("Tienda")]
         public int TiendaId { get; set; }
+        [ForeignKey("PuntoDeRecoleccion")]
+        public int PuntoDeRecoleccionId { get; set; }
     }
 }
