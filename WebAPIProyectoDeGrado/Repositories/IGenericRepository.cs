dba300d57fd7f38effe07968237eee5ff35fd093
esteban14658 <esteban_14658@hotmail.com>
2021-08-12 10:20:24 -0500

    Estoy agregando interfaces

---
+++ WebAPIProyectoDeGrado/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIProyectoDeGrado.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity: class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+        Task<TEntity> Insert(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(int id);
+    }
+}
