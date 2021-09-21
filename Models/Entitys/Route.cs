0f59d48abc5092c377972b8e24a8a46b8009907f
esteban14658 <esteban_14658@hotmail.com>
2021-09-21 08:46:11 -0500

    Se agregaron las dos entidades faltantes

---
+++ Models/Entitys/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIProyectoDeGrado.Entitys;
+
+namespace PG.Models.Entitys
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<CollectionPoint> CollectionPoints { get; set; }
+        public Comment Comment { get; set; }
+        [ForeignKey("Recycler")]
+        public int RecyclerId { get; set; }
+
+    }
+}
