a475be7debb3433e7563353dfb249c7c51e6f0d4
RODRIGO BELTRAN <esteban_14658@hotmail.com>
2022-03-06 18:59:44 -0500

    Agregado el insertar productos a la tienda

---
+++ WebAPIProyectoDeGrado/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PG.Bussiness.DTOs.GetDTOs;
+using PG.Bussiness.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PG.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost("{idShop:int}")]
+        public async Task<ActionResult<List<OrderDTO>>> Post([FromBody] List<OrderDTO> orderList, int idShop)
+        {
+            var orders = await _orderService.AddOrderToShop(idShop, orderList);
+            return Created("", orders);
+        }
+    }
+}
